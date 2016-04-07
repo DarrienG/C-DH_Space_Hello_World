3e5cd09a9ea17508826b63865e48b49a45c18f1d
Darrien Glasser <darrienglasser@outlook.com>
2016-04-06 21:55:04 -0400

    Now fully functional. Let's not delete your work this time

--- PowerCell.cpp
+++ PowerCell.cpp
@@ -14,15 +14,15 @@ using std::endl;
 
 /*!
  * Public constructor. Takes a vector of PowerTeam, and gets the total voltage
- * from it. 
+ * from it.
  */
 PowerCell::PowerCell(vector<PowerTeam> powerCell) {
-	mPowerCell = powerCell;
-	mTotalVoltage = 0;
-	for (int i = 0; i < mPowerCell.size(); ++i)
-	{
-		mTotalVoltage += mPowerCell[i].getVoltage();
-	}
+    mPowerCell = powerCell;
+    mTotalVoltage = 0;
+    for (int i = 0; i < mPowerCell.size(); ++i)
+    {
+        mTotalVoltage += mPowerCell[i].getVoltage();
+    }
 }
 
 /*!
@@ -31,43 +31,43 @@ PowerCell::PowerCell(vector<PowerTeam> powerCell) {
  * @return The total voltage across all power cells.
  */
 double PowerCell::getTotalVoltage() {
-	return mTotalVoltage;
+    return mTotalVoltage;
 }
 
 /*!
  * Takes power from the PowerCell. Prints an error message if current draw is
- * too high. 
+ * too high.
  */
 void PowerCell::drawPower(double power) {
-	if (power > mTotalVoltage)
-	{
-		cout << "Power draw too high, cannot draw: " << power << "with only " \
-		<< mTotalVoltage << " power left." << endl;
-	}
+    if (power > mTotalVoltage)
+    {
+        cout << "Power draw too high, cannot draw: " << power << "with only " \
+        << mTotalVoltage << " power left." << endl;
+    }
 
-	// While we still have power, and we haven't moved past our final cell,
-	// keep drawing power from each cell.
-	for (int i = 0; mTotalVoltage - power != 0 && i < mPowerCell.size(); ++i)
-	{
-		double powerDrawn;
-		if (power > mPowerCell[i].getVoltage())
-		{
-			powerDrawn = mPowerCell[i].getVoltage();
-		} else {
-			powerDrawn = power;
-		}
-		mPowerCell[i].setVoltage(mPowerCell[i].getVoltage() - powerDrawn);
-		mTotalVoltage -= powerDrawn;
-	}
-	printCell();
+    // While we still have power, and we haven't moved past our final cell,
+    // keep drawing power from each cell.
+    for (int i = 0; power != 0; ++i)
+    {
+        double powerDrawn;
+        if (power > mPowerCell[i].getVoltage())
+        {
+            powerDrawn = mPowerCell[i].getVoltage();
+        } else {
+            powerDrawn = power;
+        }
+        mPowerCell[i].setVoltage(mPowerCell[i].getVoltage() - powerDrawn);
+        mTotalVoltage -= powerDrawn;
+        power -= powerDrawn;
+    }
 }
 
 void PowerCell::printCell() {
-	cout << "Total power left: " << mTotalVoltage << endl;
-	cout << "Power in each cell: " << endl;
-	for (int i = 0; i < mPowerCell.size(); ++i)
-	{
-		cout << "Battery " << i + 1 << ": " << mPowerCell[i].getVoltage() \
-		<< endl;
-	}
+    cout << "Total power left: " << mTotalVoltage << endl;
+    cout << "Power in each cell: " << endl;
+    for (int i = 0; i < mPowerCell.size(); ++i)
+    {
+        cout << "Battery " << i + 1 << ": " << mPowerCell[i].getVoltage() \
+        << endl;
+    }
 }
